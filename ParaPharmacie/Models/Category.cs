efa2486b11539318228a27ff8ef2d06442a4cfbf
FNF001 <naderfarid.07@gmail.com>
2022-09-02 16:51:17 +0100

    Add Category / Update Product

---
+++ ParaPharmacie/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParaPharmacie.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CatId { get; set; }
+        [Required(ErrorMessage ="Ce Champ est obligatoire")]
+        public string? CatName { get; set; }
+        public string? CatPhoto { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
--- ParaPharmacie/Models/Product.cs
+++ ParaPharmacie/Models/Product.cs
@@ -1,8 +1,23 @@
-﻿namespace ParaPharmacie.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParaPharmacie.Models
 {
     public class Product
     {
 
+        [Key]
         public int ProId { get; set; }
+        [Required(ErrorMessage ="Ce champ est obligatoire")]
+        public string? ProName { get; set; }
+        [Required(ErrorMessage = "Ce champ est obligatoire")]
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "Ce champ est obligatoire")]
+        public decimal Price { get; set; }
+
+        public string ProImage { get; set; }
+
+        public IFormFile File { get; set; }
+
     }
 }
