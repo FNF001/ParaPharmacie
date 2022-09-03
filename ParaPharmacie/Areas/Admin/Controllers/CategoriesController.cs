c09ff822969b018e153acbefd77c581021e09084
FNF001 <naderfarid.07@gmail.com>
2022-09-03 16:24:40 +0100

    Add Create and Edit Category

---
+++ ParaPharmacie/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParaPharmacie.Models;
+
+namespace ParaPharmacie.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Category model)
+        {
+            return View();
+        }
+
+        
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
