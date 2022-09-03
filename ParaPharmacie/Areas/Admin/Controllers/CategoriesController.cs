ee08ee1cd1d4df5cab8548317d6ef8b13fe245a5
FNF001 <naderfarid.07@gmail.com>
2022-09-03 21:47:48 +0100

    Add Category OK / Edit Category OK / Delete Category  OK

--- ParaPharmacie/Areas/Admin/Controllers/CategoriesController.cs
+++ ParaPharmacie/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ParaPharmacie.Data;
 using ParaPharmacie.Models;
 
 namespace ParaPharmacie.Areas.Admin.Controllers
@@ -6,40 +7,81 @@ namespace ParaPharmacie.Areas.Admin.Controllers
     [Area("Admin")]
     public class CategoriesController : Controller
     {
+        private readonly EcommerceContext _context;
+        public CategoriesController(EcommerceContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories.ToList();
+            return View(categories);
         }
-        
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
-        
+
         [HttpPost]
-        public IActionResult Create(Category model)
+        public async Task<IActionResult> Create(Category model, IFormFile File)
         {
-            return View();
+            if (File != null)
+            {
+                string imageName = Guid.NewGuid().ToString() + ".jpg";
+                string filePathImage = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\category", imageName);
+                using (var stream = System.IO.File.Create(filePathImage))
+                {
+                    await File.CopyToAsync(stream);
+                }
+                model.CatPhoto = imageName;
+            }
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
-        
+
         [HttpGet]
-        public IActionResult Edit()
+        public IActionResult Edit(int? id)
         {
-            return View();
+            var categories = _context.Categories.Find(id);
+            return View(categories);
         }
-        
+
         [HttpPost]
-        public IActionResult Edit(Category model)
+        public IActionResult Edit(int id, Category model, IFormFile File)
         {
-            return View();
+            if (File != null)
+            {
+                string imageName = Guid.NewGuid().ToString() + ".jpg";
+                string filePathImage = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\category", imageName);
+                using (var stream = System.IO.File.Create(filePathImage))
+                {
+                    File.CopyTo(stream);
+                }
+                model.CatPhoto = imageName;
+            }
+            else
+            {
+                model.CatPhoto = model.CatPhoto;
+            }
+            _context.Update(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
-
+    
         
-        public IActionResult Delete()
+        public IActionResult Delete(int? id)
+        
         {
-            return View();
-        }
-
+                if (id != null)
+                {
+                var cat = _context.Categories.Find(id);
+                _context.Categories.Remove(cat);
+                _context.SaveChanges();
+                }
+            return RedirectToAction("Index");
+        }        
     }
 }
